6cb2a6943d0a685ad1309181e58f9509a7fde15c
Pravin Pimple <ppimple@internal.tavisca.com>
2017-03-31 20:31:09 +0530

    Initial commit with Titanium web proxy integration.

---
+++ MIM Mocker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MIM_Mocker
+{
+    class Program
+    {
+        private static readonly ProxyController Controller = new ProxyController();
+
+        public static void Main(string[] args)
+        {
+            //On Console exit make sure we also exit the proxy
+            NativeMethods.Handler = ConsoleEventCallback;
+            NativeMethods.SetConsoleCtrlHandler(NativeMethods.Handler, true);
+
+            Controller.StartProxy();
+
+            Console.WriteLine("Hit any key to shutdown MIM Mocker proxy server.");
+            Console.WriteLine();
+            Console.Read();
+
+            Controller.Stop();
+        }
+
+        private static bool ConsoleEventCallback(int eventType)
+        {
+            if (eventType != 2) return false;
+            try
+            {
+                Controller.Stop();
+            }
+            catch
+            {
+                // ignored
+            }
+            return false;
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        // Keeps it from getting garbage collected
+        internal static ConsoleEventDelegate Handler;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+
+        // Pinvoke
+        internal delegate bool ConsoleEventDelegate(int eventType);
+    }
+}
---
+++ MIM Mocker/ProxyController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace MIM_Mocker
+{
+    public class ProxyController
+    {
+        private ProxyServer proxyServer;
+
+        public ProxyController()
+        {
+            proxyServer = new ProxyServer();
+            proxyServer.TrustRootCertificate = true;
+        }
+
+        public void StartProxy()
+        {
+            proxyServer.BeforeRequest += OnRequest;
+            proxyServer.BeforeResponse += OnResponse;
+            proxyServer.ServerCertificateValidationCallback += OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback += OnCertificateSelection;
+
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true)
+            {
+                //Exclude Https addresses you don't want to proxy
+                //Usefull for clients that use certificate pinning
+                //for example dropbox.com
+                // ExcludedHttpsHostNameRegex = new List<string>() { "google.com", "dropbox.com" }
+
+                //Use self-issued generic certificate on all https requests
+                //Optimizes performance by not creating a certificate for each https-enabled domain
+                //Usefull when certificate trust is not requiered by proxy clients
+                // GenericCertificate = new X509Certificate2(Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "genericcert.pfx"), "password")
+            };
+
+            //An explicit endpoint is where the client knows about the existance of a proxy
+            //So client sends request in a proxy friendly manner
+            proxyServer.AddEndPoint(explicitEndPoint);
+            proxyServer.Start();
+
+
+            //Transparent endpoint is usefull for reverse proxying (client is not aware of the existance of proxy)
+            //A transparent endpoint usually requires a network router port forwarding HTTP(S) packets to this endpoint
+            //Currently do not support Server Name Indication (It is not currently supported by SslStream class)
+            //That means that the transparent endpoint will always provide the same Generic Certificate to all HTTPS requests
+            //In this example only google.com will work for HTTPS requests
+            //Other sites will receive a certificate mismatch warning on browser
+            var transparentEndPoint = new TransparentProxyEndPoint(IPAddress.Any, 8001, true)
+            {
+                GenericCertificateName = "google.com"
+            };
+            proxyServer.AddEndPoint(transparentEndPoint);
+
+            //ProxyServer.UpStreamHttpProxy = new ExternalProxy() { HostName = "localhost", Port = 8888 };
+            //ProxyServer.UpStreamHttpsProxy = new ExternalProxy() { HostName = "localhost", Port = 8888 };
+
+            foreach (var endPoint in proxyServer.ProxyEndPoints)
+                Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
+                    endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+
+            //Only explicit proxies can be set as system proxy!
+            proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+            proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+        }
+
+        public void Stop()
+        {
+            proxyServer.BeforeRequest -= OnRequest;
+            proxyServer.BeforeResponse -= OnResponse;
+            proxyServer.ServerCertificateValidationCallback -= OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback -= OnCertificateSelection;
+
+            proxyServer.Stop();
+        }
+
+        public async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            if (e.WebSession.Request.RequestUri.AbsoluteUri.Contains("http://qa-data.oski.tavisca.com/data/api/v1.0/healthcheck"))
+            {
+                await e.Ok("<!DOCTYPE html>" +
+                      "<html><body><h1>" +
+                      "Response from MIM Mocker" +
+                      "</h1>" +
+                      "<p>Blocked by ManInTheMiddle Mocker !</p>" +
+                      "</body>" +
+                      "</html>");
+            }
+        }
+
+        public async Task OnResponse(object sender, SessionEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Allows overriding default certificate validation logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public Task OnCertificateValidation(object sender, CertificateValidationEventArgs e)
+        {
+            //set IsValid to true/false based on Certificate Errors
+            if (e.SslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                e.IsValid = true;
+            }
+
+            return Task.FromResult(0);
+        }
+
+        /// <summary>
+        /// Allows overriding default client certificate selection logic during mutual authentication
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public Task OnCertificateSelection(object sender, CertificateSelectionEventArgs e)
+        {
+            //set e.clientCertificate to override
+
+            return Task.FromResult(0);
+        }
+    }
+}
